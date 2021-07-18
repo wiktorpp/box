f7a08b1275392bb9898b58c55c90211e84a71cb8
wiktorpp <grazynapp65@gmail.com>
2021-07-18 15:41:28 +0200

    box

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from box import box
 print(box("Box!"))
-print(box(["Box,", "box box box!"]))
+print(box(["Box!", "Box box box!"]))
 print(box((10,10)))
-print(box("Box,\nbox box box!"))
+print(box("Box!\nBox box box!"))
 print(box())
